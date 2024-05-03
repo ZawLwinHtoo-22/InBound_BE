8d452f37fda53d926d69add567a2218300b1186d
ZawLwinHtoo-22 <zawlwinhtoo514@gmail.com>
2024-05-03 11:47:05 +0630

    first commit

---
+++ src/main/java/com/inbound/inbound_be/InboundBeApplication.java
@@ -0,0 +1,13 @@
+package com.inbound.inbound_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboundBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InboundBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inbound/inbound_be/InboundBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inbound.inbound_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboundBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
