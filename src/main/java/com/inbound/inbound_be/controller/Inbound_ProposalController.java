9944c06fef40297676841990d58953dd54980a44
ShineHtetWai29 <familyshien293@gmail.com>
2024-05-15 13:17:06 +0630

    update

--- src/main/java/com/inbound/inbound_be/controller/Inbound_ProposalController.java
+++ src/main/java/com/inbound/inbound_be/controller/Inbound_ProposalController.java
@@ -38,25 +38,25 @@ public class Inbound_ProposalController {
     @Autowired
     private AgentRepo agentRepo;
     @PostMapping
-    public ResponseEntity<InBound_Proposal> createProposal(@RequestBody Inbound_ProposalRequest request){
-        InBound_Proposal inBoundProposal=inboundProposalService.createProposal(request);
-        BeneficiaryRq rq=new BeneficiaryRq(request.getB_name(),request.getB_date(),request.getB_nrc(),request.getB_rs(),
-                request.getB_ph(),request.getB_email(),request.getB_address(),request.getR_country_b());
-        Beneficiary beneficiary=beneficiaryService.addBeneficiary(rq);
+    public ResponseEntity<InBound_Proposal> createProposal(@RequestBody Inbound_ProposalRequest request) {
+        InBound_Proposal inBoundProposal = inboundProposalService.createProposal(request);
+        BeneficiaryRq rq = new BeneficiaryRq(request.getB_name(), request.getB_date(), request.getB_nrc(), request.getB_rs(),
+                request.getB_ph(), request.getB_email(), request.getB_address(), request.getR_country_b());
+        Beneficiary beneficiary = beneficiaryService.addBeneficiary(rq);
 
-            InsuredPersonRq insuredPersonRq=new InsuredPersonRq(request.getI_name(),request.getI_dob(),request.getI_gender(),
-                    request.getI_phone(),request.getI_email(),request.getI_person_address(),request.getR_person_address(),
-                    request.getPassportNumber(),request.getI_passport_issue_date(),request.getIsChild(),request.getPassportCountry(),beneficiary.getB_id(),request.getR_country_i());
-            InsuredPerson insuredPerson=insuredPersonService.addInsuredPerson(insuredPersonRq);
+        InsuredPersonRq insuredPersonRq = new InsuredPersonRq(request.getI_name(), request.getI_dob(), request.getI_gender(),
+                request.getI_phone(), request.getI_email(), request.getI_person_address(), request.getR_person_address(),
+                request.getPassportNumber(), request.getI_passport_issue_date(), request.getIsChild(), request.getPassportCountry(), beneficiary.getB_id(), request.getR_country_i());
+        InsuredPerson insuredPerson = insuredPersonService.addInsuredPerson(insuredPersonRq);
         inBoundProposal.setInsuredPerson(insuredPerson);
-        if(request.getIsChild() == true){
-            ChildRequest childRequest=new ChildRequest(request.getCh_name(),request.getCh_dob(),request.getCh_gender(),
-                    request.getCh_gu_Name(),request.getCh_rs(),insuredPerson.getI_person_id());
-            Child child=childService.addChild(childRequest);
-        }else {
+        if (request.getIsChild() == true) {
+            ChildRequest childRequest = new ChildRequest(request.getCh_name(), request.getCh_dob(), request.getCh_gender(),
+                    request.getCh_gu_Name(), request.getCh_rs(), insuredPerson.getI_person_id());
+            Child child = childService.addChild(childRequest);
+        } else {
             System.out.println("No child");
         }
-        Agent agent=agentRepo.findAgentByLicenseNo(request.getLicenseNo());
+        Agent agent = agentRepo.findAgentByLicenseNo(request.getLicenseNo());
         inBoundProposal.setAgent(agent);
         inBoundProposal.setP_st_date(LocalDate.now());
         inBoundProposal.setP_en_date(LocalDate.now().plusDays(request.getCo_plan()));
@@ -65,7 +65,7 @@ public class Inbound_ProposalController {
         inBoundProposal.setUpdatedDate(LocalDate.now());
         inBoundProposal.setVersion(1);
         return new ResponseEntity<>(inboundProposalRepo.save(inBoundProposal), HttpStatus.CREATED);
-    }gi
+    }
 
     @GetMapping
     public ResponseEntity<List<InBound_Proposal>> showAll(){
